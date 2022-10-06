0550025115f19fa36746a8008b9acd77c0444792
erika-h7 <erika.herrera1@outlook.com>
2022-10-06 14:59:01 -0700

    Initial Commit

---
+++ ErikaCard/ErikaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErikaCardApp.swift
+//  ErikaCard
+//
+//  Created by Infinity Code on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ErikaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
